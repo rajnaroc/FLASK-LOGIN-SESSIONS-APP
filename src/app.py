3a594a45c96d9adc2b6e05e967d2e193b7560639
Aaron Campos <aaroncm611@gmail.com>
2025-01-23 14:19:55 +0100

    Hemos añadido el hash al login y la BD

--- src/app.py
+++ src/app.py
@@ -1,14 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect,url_for
 from config import config
+from flask_mysqldb import MYSQL
 
 app = Flask(__name__)
 
+db = MYSQL(app)
+
 @app.route('/')
 def index():
+    return redirect(url_for('login'))
 
-    return render_template('auth/login.html')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        print(request.form['username'])    
+        print(request.form['password'])    
+
+        return 'DATOS PROCESADOS'
+    
+    else:
+        return render_template('auth/login.html')
 
 if __name__ == '__main__':
 
     app.config.from_object(config['development'])
-    app.run(debug=True)
+    app.run()
